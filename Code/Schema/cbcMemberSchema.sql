ea37f34b21fd95702722148ece848b42ddebba8b
mduby <mduby@broadinstitute.org>
2015-10-04 14:57:26 -0400

    added racks

--- Code/Schema/cbcMemberSchema.sql
+++ Code/Schema/cbcMemberSchema.sql
@@ -23,6 +23,7 @@ create table member_status (
 -- insert rows into member status table
 insert into member_status values(1, 'Active', 'Member in active status, can use all facilities');
 insert into member_status values(2, 'Inactive', 'Member in inactive status, cannot use all facilities');
+insert into member_status values(3, 'Wait list', 'Member in inactive wait list status, cannot use all facilities');
 
 
 -- member type lookup table
@@ -37,10 +38,32 @@ insert into member_type values(1, 'Rowing member', 'Member is a rowing member, t
 insert into member_type values(2, 'Social member', 'Member is a social member, to be billed accordingly');
     
 
+-- rack type lookup table
+drop table if exists rack_type;
+create table rack_type (
+    rack_type_id int(9) not null,
+    web_name varchar(1000) not null,
+    description varchar(4000));
+
+-- insert rows into rack type table
+insert into rack_type values(1, 'Indoor rack', 'A full time rack contained within the walls of the building');
+insert into rack_type values(2, 'Outdoor rack', 'A full time rack not contained within the walls of the building');
+insert into rack_type values(3, 'Temporary rack', 'A rack only used for temporary storage');
 
 
 
 -- transactional tables (will be modified regularly)
+-- member application table
+drop table if exists member_application;
+create table member_application (
+    member_application_id int(9) not null auto_increment primary key,
+    first_name varchar(4000) not null,
+    middle_name varchar(4000),
+    last_name varchar(4000) not null,
+    version bigint(19) not null default 0,
+    date_created timestamp,
+    last_updated date);
+
 -- member table
 drop table if exists member;
 create table member (
