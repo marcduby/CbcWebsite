2df20ef53c6984baa8553af8f8430ac6c066a4da
mduby <mduby@broadinstitute.org>
2015-12-20 21:32:45 -0500

    added download scripts

---
+++ Code/Schema/downloadAddress.sql
@@ -0,0 +1,4 @@
+
+
+select member_id, address_street1, address_street2, address_city, address_state, address_zip_code, address_country, address_phone_primary, address_phone_secondary from member;
+    
---
+++ Code/Schema/downloadEmail.sql
@@ -0,0 +1,5 @@
+
+
+select member_id, member_since, username, email, password from member;
+
+
